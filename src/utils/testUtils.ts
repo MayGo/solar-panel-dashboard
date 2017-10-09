a66b486c4505b4b486adeb92927ae0745275d424
Maigo Erit <maigo.erit@smit.ee>
2017-10-09 23:55:43 +0300

    Add solarpanel test data

--- src/utils/testUtils.ts
+++ src/utils/testUtils.ts
@@ -1,14 +1,53 @@
 import IPvPanel from '../@types/IPvPanel';
 
+const pvPanelNames = [
+  'A1B',
+  'V2S',
+  'B3N',
+  'V4S',
+  'R5S',
+  'U6Y',
+  'D7R',
+  'D8R',
+  'D9R',
+  'D10R',
+  'G11B',
+  'R12S',
+  'Q13N',
+  'A9S',
+  'B9S',
+  'L0C',
+  'E1D',
+  'E2E',
+  'E3F',
+  'E4G',
+  'N1H',
+  'L3I',
+  'C3J',
+  'P9K',
+  'A9L',
+  'P0M',
+  'V1N',
+  'V2O',
+  'V3P',
+  'V4Q',
+];
 export const returnPvOutputMock = (): IPvPanel[] => {
   const items: IPvPanel[] = [];
-  const item: IPvPanel = {
-    id: 'A12',
-    voltage: 123,
-    wattage: 43355,
-  };
-  items.push(item);
+  pvPanelNames.forEach(id => {
+    const item: IPvPanel = {
+      id,
+      voltage: getRandomArbitrary(50, 400),
+      wattage: getRandomArbitrary(10000, 50000),
+    };
+    items.push(item);
+  });
+
   return items;
+};
+
+const getRandomArbitrary = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 export const transformConsoleMessagesToExceptions = () => {
